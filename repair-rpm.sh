0c9d67b0452f1c69022fb9ec2b84b8c257ad4260
Andrew Pennebaker <andrew.pennebaker@networkedinsights.com>
2015-06-05 15:52:44 -0500

    debugging unsupported hash version number 8

---
+++ repair-rpm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+repair_db() {
+  filename="$1"
+
+  echo "Repairing ${filename}..."
+
+  backup="${filename}.orig"
+  mv $filename $backup
+  db40_dump $backup | db31_load $filename
+  rm $backup
+}
+
+rm -rf /chroot/var/lib/rpm/__db*
+
+for f in $(find /chroot/var/lib/rpm -name '[A-Z]*'); do
+  repair_db "$f"
+done
+
+chroot /chroot rpm -v --rebuilddb
